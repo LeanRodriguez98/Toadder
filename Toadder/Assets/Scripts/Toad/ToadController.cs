d03b1c804b0608ff7246ac376df04ec2f8bcb4db
Unknown <lean1998@hotmail.com>
2018-07-29 19:31:39 -0300

    Particles

--- Toadder/Assets/Scripts/Toad/ToadController.cs
+++ Toadder/Assets/Scripts/Toad/ToadController.cs
@@ -8,16 +8,22 @@ public class ToadController : MonoBehaviour
     public float movementTolerance;
     public float jumpForce;
     public float JumpDistance;
+    public float timeDeadCamera;
+    public int lifes;
+    public GameObject bloodParticles;
+    public GameObject whaterParticles;
     public List<Vector3> characterRotations;
     public List<GameObject> MovementColliders;
     private float jumpStartHeight;
     private float tableSpeed;
+    private float auxTimeDeadCamera;
     private bool inputMovement;
     private bool tableMovement;
     private bool directionTable;
+    private bool lifeDown;
     private Vector3 position;
     private Vector3 auxPosition;
-    
+    private Vector3 startPosition;
     void Awake()
     {
         instance = this;
@@ -28,96 +34,127 @@ public class ToadController : MonoBehaviour
     {
         inputMovement = true;
         tableMovement = false;
+        lifeDown = false;
         position = transform.position;
-        auxPosition = transform.position;      
+        auxPosition = transform.position;
+        startPosition = transform.position;
+        auxTimeDeadCamera = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        if(lifeDown)
+        LifeDown();
     }
 
     void Movement()
     {
         auxPosition.y = transform.position.y;
-
-        if (inputMovement)
+        if (!lifeDown)
         {
-            if (Input.GetKeyDown(KeyCode.W) && !MovementColliders[0].gameObject.GetComponent<MovementColliders>().trigger)
+
+
+            if (inputMovement)
             {
-                auxPosition.z += JumpDistance;
-                transform.eulerAngles = characterRotations[0];
-                tableMovement = false;
-            }
-            if (Input.GetKeyDown(KeyCode.S) && !MovementColliders[1].gameObject.GetComponent<MovementColliders>().trigger)
-            {
-                auxPosition.z -= JumpDistance;
-                transform.eulerAngles = characterRotations[2];
-                tableMovement = false;
-            }
-            if (Input.GetKeyDown(KeyCode.A) && !MovementColliders[2].gameObject.GetComponent<MovementColliders>().trigger)
-            {
-                auxPosition.x -= JumpDistance;
-                transform.eulerAngles = characterRotations[3];
-                tableMovement = false;
-            }
-            if (Input.GetKeyDown(KeyCode.D) && !MovementColliders[3].gameObject.GetComponent<MovementColliders>().trigger)
-            {
-                auxPosition.x += JumpDistance;
-                transform.eulerAngles = characterRotations[1];
-                tableMovement = false;
+                if (Input.GetKeyDown(KeyCode.W) && !MovementColliders[0].gameObject.GetComponent<MovementColliders>().trigger)
+                {
+                    auxPosition.z += JumpDistance;
+                    transform.eulerAngles = characterRotations[0];
+                    tableMovement = false;
+                }
+                if (Input.GetKeyDown(KeyCode.S) && !MovementColliders[1].gameObject.GetComponent<MovementColliders>().trigger)
+                {
+                    auxPosition.z -= JumpDistance;
+                    transform.eulerAngles = characterRotations[2];
+                    tableMovement = false;
+                }
+                if (Input.GetKeyDown(KeyCode.A) && !MovementColliders[2].gameObject.GetComponent<MovementColliders>().trigger)
+                {
+                    auxPosition.x -= JumpDistance;
+                    transform.eulerAngles = characterRotations[3];
+                    tableMovement = false;
+                }
+                if (Input.GetKeyDown(KeyCode.D) && !MovementColliders[3].gameObject.GetComponent<MovementColliders>().trigger)
+                {
+                    auxPosition.x += JumpDistance;
+                    transform.eulerAngles = characterRotations[1];
+                    tableMovement = false;
+                }
+
+                if (auxPosition.x > position.x + movementTolerance ||
+                    auxPosition.x < position.x - movementTolerance ||
+                    auxPosition.z > position.z + movementTolerance ||
+                    auxPosition.z < position.z - movementTolerance)
+                {
+                    inputMovement = false;
+                    jumpStartHeight = transform.position.y;
+                }
+                position.y = transform.position.y;
             }
 
-            if (auxPosition.x > position.x + movementTolerance ||
-                auxPosition.x < position.x - movementTolerance ||
-                auxPosition.z > position.z + movementTolerance ||
-                auxPosition.z < position.z - movementTolerance)
+            if (!inputMovement)
             {
-                inputMovement = false;
-                jumpStartHeight = transform.position.y;
+                UpdateJumpHeight();
+
+                if (auxPosition.x > position.x + movementTolerance)
+                    position.x += Time.deltaTime;
+                else if (auxPosition.x < position.x - movementTolerance)
+                    position.x -= Time.deltaTime;
+                else if (auxPosition.z > position.z + movementTolerance)
+                    position.z += Time.deltaTime;
+                else if (auxPosition.z < position.z - movementTolerance)
+                    position.z -= Time.deltaTime;
+                else
+                    inputMovement = true;
+
             }
-            position.y = transform.position.y;
-        }
 
-        if (!inputMovement)
-        {
-            UpdateJumpHeight();
-
-            if (auxPosition.x > position.x + movementTolerance)            
-                position.x += Time.deltaTime;            
-            else if (auxPosition.x < position.x - movementTolerance)            
-                position.x -= Time.deltaTime;            
-            else if (auxPosition.z > position.z + movementTolerance)            
-                position.z += Time.deltaTime;            
-            else if (auxPosition.z < position.z - movementTolerance)            
-                position.z -= Time.deltaTime;            
-            else            
-                inputMovement = true;           
-            
-        }
-
-        if (tableMovement)
-        {
-            if (directionTable)
+            if (tableMovement)
             {
-                transform.position += Vector3.right * Time.deltaTime * tableSpeed;
+                if (directionTable)
+                {
+                    transform.position += Vector3.right * Time.deltaTime * tableSpeed;
+                }
+                else
+                {
+                    transform.position += Vector3.left * Time.deltaTime * tableSpeed;
+                }
+                position = transform.position;
+                auxPosition = position;
+
             }
             else
             {
-                transform.position += Vector3.left * Time.deltaTime * tableSpeed;
+                transform.position = position;
             }
-            position = transform.position;
-            auxPosition = position;
-
         }
-        else
-        {
-            transform.position = position;
-        }
-
     }
 
+    void LifeDown()
+    {
+        auxTimeDeadCamera += Time.deltaTime;
+        if (auxTimeDeadCamera >= timeDeadCamera)
+        {
+            lifes--;
+            transform.position = startPosition;
+            position = transform.position;
+            auxPosition = transform.position;
+            auxTimeDeadCamera = 0;
+            lifeDown = false;
+            tableMovement = false;
+
+            if (lifes <= 0)
+            {
+                Destroy(gameObject);
+            }
+            GetComponentInChildren<MeshRenderer>().enabled = true;
+
+        }
+
+
+    }
 
     void UpdateJumpHeight()
     {
@@ -130,9 +167,17 @@ public class ToadController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Whater"||collision.gameObject.tag == "Car")
+        if (collision.gameObject.tag == "Whater")
         {
-            Destroy(gameObject);   
+            lifeDown = true;
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+            Instantiate(whaterParticles, transform.position, Quaternion.identity);
+        }
+        if (collision.gameObject.tag == "Car")
+        {
+            lifeDown = true;
+            GetComponentInChildren<MeshRenderer>().enabled = false;
+            Instantiate(bloodParticles,transform.position,Quaternion.identity);
         }
         if (collision.gameObject.tag == "Table")
         {
@@ -148,5 +193,7 @@ public class ToadController : MonoBehaviour
             }
         }
     }
+    
+
 }
 
