fa7b2978e56382102d921883bec4ea7b531564d5
Unknown <lean1998@hotmail.com>
2018-08-05 21:56:10 -0300

    Highscores

---
+++ Toadder/Assets/Scripts/HighscoreManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HighscoreManager : MonoBehaviour {
+
+    public static HighscoreManager Instancie;
+    [HideInInspector] public List<int> ScoreList = new List<int>();
+    [HideInInspector] public bool AsignHishcore;
+    public int CantHighscores;
+    public Object LoadingScenes;
+    public Object[] Levels;
+
+    void Awake()
+    {
+        if (Instancie == null)
+        {
+            Instancie = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+        AsignHishcore = true;
+        for (int i = 0; i < CantHighscores; i++)
+        {
+            ScoreList.Add(0);
+        }
+    }
+
+
+    void Update()
+    {
+
+        if (SceneManager.GetActiveScene().name == LoadingScenes.name && !AsignHishcore)
+        {
+            AsignHishcore = true;
+        }
+
+        for (int i = 0; i < Levels.Length; i++)
+        {
+            if (ToadController.instance.lifes <= 0 && AsignHishcore && SceneManager.GetActiveScene().name == Levels[i].name)
+            {
+
+                ScoreList.Add(PlayerStats.Instancie.Points);
+                ScoreList.Sort();
+                ScoreList.Reverse();
+                AsignHishcore = false;
+            }
+        }
+    }
+}
+
+
---
+++ Toadder/Assets/Scripts/HighscoreText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighscoreText : MonoBehaviour {
+
+    public Text[] HighscoreTexts;
+
+    void Start()
+    {
+        if (HighscoreManager.Instancie != null)
+        {
+            for (int i = 0; i < HighscoreTexts.Length; i++)
+            {
+                HighscoreTexts[i].text = HighscoreManager.Instancie.ScoreList[i].ToString("0000");
+            }
+        }
+    }
+}
