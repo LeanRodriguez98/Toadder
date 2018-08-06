1892d3bd05da3927e4eb4531c04a2dc0cc4ea474
Unknown <lean1998@hotmail.com>
2018-08-06 01:12:20 -0300

    Dressing + FoldersOrder + GeneralFixes

---
+++ Toadder/Assets/Scripts/LoadingScenes/BackgroundToadMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundToadMovement : MonoBehaviour {
+    private Rigidbody rb;
+    private Vector3 position;
+    private Vector3 addForce;
+    public float timer;
+    public float jumpForce;
+    private float auxTimer;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        position = transform.position;
+        addForce.x = 1;
+        addForce.y = 1;
+        addForce.z = 0;
+        auxTimer = timer;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        auxTimer += Time.deltaTime;
+        if (auxTimer > timer)
+        {
+            rb.AddForce(addForce * jumpForce);
+            auxTimer = 0;
+        }
+        
+    }
+}
